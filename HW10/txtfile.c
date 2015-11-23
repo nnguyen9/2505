00eea1692edfaef99518dfac7d0bfb79ee539cee
Nam Nguyen <nnguyen9@vt.edu>
2015-11-23 10:49:09 -0500

    Read

--- HW10/txtfile.c
+++ HW10/txtfile.c
@@ -18,7 +18,7 @@
 //
 bool txtfile_read(FILE *in, string_t * const result)
 {
-	string_init(&result);
+	string_init(result);
 	while(!feof(in)) {
 		char* buff = malloc(sizeof(char)* (LINE_CHUNK));
 		fgets(buff, LINE_CHUNK, in);
@@ -74,3 +74,18 @@ bool txtfile_readlines(FILE *in, string_t ** const result, size_t * num_lines)
 	return true;
 }
 
+//On my honor:
+//
+//-I have not discussed the C language code in my program with
+// anyone other than my instructor or the teaching assistants 
+// assigned to this course.
+//
+//-I have not used C language code obtained from another student, 
+// or any other unauthorized source, either modified or unmodified.  
+//
+//-If any C language code or documentation used in my program 
+// was obtained from an allowed source, such as a text book or course
+// notes, that has been clearly noted with a proper citation in
+// the comments of my program.
+//
+// Nam Nguyen
